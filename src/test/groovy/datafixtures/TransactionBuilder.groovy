95a6a82d10e379c1b0a62d0ebb5c1218a7c6080c
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-12-02 17:10:00 -0500

    Create test builders

--- src/test/groovy/datafixtures/TransactionBuilder.groovy
+++ src/test/groovy/datafixtures/TransactionBuilder.groovy
@@ -1,28 +1,32 @@
 package datafixtures
 
+import com.kinandcarta.transactionsapi.entity.Account
+import com.kinandcarta.transactionsapi.entity.Transaction
 
-import com.kinandcarta.transactionsapi.domain.response.TransactionResponse
+import java.time.LocalDate
 
 import static utils.RandomGenerator.randomLong
-import static utils.RandomGenerator.randomBigDecimal
+import static utils.RandomGenerator.randomDouble
 import static utils.RandomGenerator.randomString
 
-class TransactionResponseBuilder {
+class TransactionBuilder {
     private long transactionId = randomLong()
-    private long date = randomLong()
-    private double amount = randomBigDecimal()
+    private long date = LocalDate.parse("2023-11-19").toEpochDay()
+    private double amount = randomDouble()
     private String merchantName = randomString()
     private String summary = randomString()
+    private Account account = new AccountBuilder().build()
 
     //@TODO: Can add the with methods here, if we want to override the random values.
 
     def build() {
-        new TransactionResponse(
+        new Transaction(
                 transactionId: transactionId,
                 date: date,
                 amount: amount,
                 merchantName: merchantName,
-                summary: summary
+                summary: summary,
+                account: account,
         )
     }
 }
