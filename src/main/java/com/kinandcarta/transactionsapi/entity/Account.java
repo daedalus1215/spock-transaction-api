6c0bcb8381bf0821e9af704da742c77486f887f8
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-12-02 16:43:52 -0500

    Create Account + Transaction - no Specs necessary

---
+++ src/main/java/com/kinandcarta/transactionsapi/entity/Account.java
@@ -0,0 +1,31 @@
+package com.kinandcarta.transactionsapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "ACCOUNTS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+    @Id
+    @Column(name = "ACCOUNT_ID")
+    private long accountId;
+
+    private String memberName;
+
+    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
+    private Set<Transaction> transactions;
+}
